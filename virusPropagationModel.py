a0a09fe80f38fa4e093cb04e513d53b34795c358
avivko <aviv.korman@gmail.com>
2020-04-11 18:07:12 +0200

    - changed the human method update status to be called update state (because more than just the status is now updated, e.g. flags too) - changed the simulation function to now use a nparray instead of a list - made the simulation function now simulate and contain all the state  changes (not just status)

--- virusPropagationModel.py
+++ virusPropagationModel.py
@@ -8,6 +8,25 @@ import numpy as np
 
 
 class ModeledPopulatedWorld(object):
+    """
+        A Class which initializes a world with location and humans (a static snapshot of its origin).
+        This class can be used as an object by the class Simulate to simulate this populated world's time course
+
+        Attributes
+        ----------
+        number_of_locs : int
+            The amount of location objects to initialize
+        number_of_people : int
+            The amount of human objects to initialize
+        initial_infections : int
+            The amount of human objects initially possibly infected (exact amount depends on a probability)
+        world: object of class World
+            the initialized world object assigned to this class object
+        locations: list of location objects of the class Location
+            the locations initialized in this world
+        people: set of human objects of the human class
+    """
+
     def __init__(self, number_of_locs, number_of_people, initial_infections):
         self.number_of_locs = number_of_locs
         self.number_of_people = number_of_people
@@ -18,6 +37,11 @@ class ModeledPopulatedWorld(object):
         self.infect(self.initial_infections)
 
     def initialize_people(self, number_of_people):  # idee martin: skalenfeiheit
+        """
+        initializes a set of people (human objects) with assigned ages and schedules
+        :param number_of_people: int. The amount of people to initialize
+        :return people: set. a set of human objects
+        """
         people = set()
         for n in range(number_of_people):
             age = random_age()
@@ -26,6 +50,12 @@ class ModeledPopulatedWorld(object):
         return people
 
     def create_schedule(self, age, locations):
+        """
+        creates a schedule, depending on a given age and locations
+        :param age: int. given age for a human from whom this schedule should be
+        :param locations: list of location objects to which the human can go
+        :return sched: dict. specifies times of transitions and assigned locations
+        """
         if 3 < age < 70:  # schedule has to depend on age, this is only preliminary
             num_locs = 5
         else:
@@ -38,37 +68,67 @@ class ModeledPopulatedWorld(object):
         return sched
 
     def infect(self, amount):
+        """
+        infects people (list of humans) at random with a certain probability
+        :param amount: int. amount of people to possibly infect
+        """
         to_infect = random.sample(self.people, amount)  # randomly choose who to infect
         for p in to_infect:
-            #p.status = 'I'
+            # p.status = 'I'
             p.get_infected(1.0, 0)
 
 
 class Simulation(object):
+    """
+        A Class which contains a simulation based on a specific ModeledPopulatedWorld object.
+
+        Attributes
+        ----------
+        modeled_populated_world : object of class ModeledPopulatedWorld
+            the initialized populated world that is to be simulated over time
+        time_steps: int
+            the amount of time steps to simulate
+        time : int
+            the last point in time the time course of this simulation
+        timecourse : list
+            contains a dicts representing a snapshot of the population (humans)
+        locations: list of location objects of the class Location
+            the locations initialized in this world
+        people: set of human objects of the human class
+    """
     def __init__(self, modeled_populated_world, time_steps):
         self.modeled_populated_world = modeled_populated_world
+        self.time_steps = time_steps
         self.time = 0
         # [{'h_ID': self.ID, 'loc': self.loc.ID, 'status': self.status, 'time': time}]
-        self.timecourse = []
-        self.time_steps = time_steps
         self.simulation_timecourse = self.run_simulation()
         self.statuses_in_timecourse = self.get_statuses_in_timecourse()
 
-    def store_state(self, person):
-        stat = person.get_status()
-        stat['time'] = self.time  #
-        self.timecourse.append(stat)
+    def get_person_attributes_per_time(self, person, only_status=False):
+        if only_status:
+            attr = person.get_status()
+        else:
+            attr = {**person.get_status(), **person.get_flags()}
+        return {**attr, **{'time': self.time}}
+
 
     def run_simulation(self):
-        for t in range(self.time_steps):
+        """
+        simulates the trajectories of all the attributes of the population
+        :return:
+        """
+        population_size = len(self.modeled_populated_world.people)
+        timecourse = np.empty(population_size*self.time_steps, dtype=object)
+        for step in range(self.time_steps):
+            person_counter = step*population_size
             self.time += 1
             for p in self.modeled_populated_world.people:  #
-                p.update_status(self.time)
+                p.update_state(self.time)
             for p in self.modeled_populated_world.people:  # don't call if hospitalized
                 p.move(self.time)
-                self.store_state(p)
-
-        return pd.DataFrame(self.timecourse)
+                timecourse[person_counter] = self.get_person_attributes_per_time(p)
+                person_counter += 1
+        return pd.DataFrame(list(timecourse))
 
     def get_statuses_in_timecourse(self):
         return list(set(self.simulation_timecourse['status']))
@@ -82,7 +142,7 @@ class Simulation(object):
             statuses = self.get_statuses_in_timecourse()
         else:
             assert set(specific_statuses) <= set(self.statuses_in_timecourse), \
-                'specified statuses ('+str(set(specific_statuses))+') dont match those in  in the population (' + \
+                'specified statuses (' + str(set(specific_statuses)) + ') dont match those in  in the population (' + \
                 str(set(self.statuses_in_timecourse)) + ')'
             statuses = specific_statuses
         status_trajectories = {}
@@ -99,27 +159,19 @@ class Simulation(object):
 
     def plot_status_timecourse(self, specific_statuses=None, save_figure=False):
         """
-        plots the timecourse for selected statuses
+        plots the time course for selected statuses
         :param save_figure:  Bool. Flag for saving the figure as an image
         :param specific_statuses:   List. Optional arg for getting only a
-        subset  of statuses. if not speficied, will plot all available statuses
+        subset  of statuses. if not specified, will plot all available statuses
         :return:
         """
-        labels = {
-            'S': 'Susceptible',
-            'R': 'Recovered',
-            'I':  'Infected',
-            'D':  'Dead'
-        }
 
         trajectories = self.get_status_trajectories(specific_statuses)
-        assert set(labels.keys()) >= set(trajectories.keys()), "label(s) missing for existing statuses in the time " \
-                                                               "course "
         simulation_timepoints = trajectories[list(trajectories.keys())[0]]['time'].values
 
         for status in trajectories.keys():
             plt.plot(simulation_timepoints,
-                     trajectories[status][status].values, label=labels[status])
+                     trajectories[status][status].values, label=status)
 
         plt.title('SecondPlot CoronaABM')
         plt.legend()
@@ -128,3 +180,8 @@ class Simulation(object):
             plt.savefig('output_plot.png')
 
 # todo: plot location ID/type cummulative timecourses for ticket #33
+
+#testing
+modeledWorld1 = ModeledPopulatedWorld(100,400,5)
+simulation1 = Simulation(modeledWorld1,100)
+simulation1.plot_status_timecourse()
