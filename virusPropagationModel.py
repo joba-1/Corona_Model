0f7d85e263bfb3561724e9380844e2484ec5428b
AmericanEagle1776 <bonnlass@hu-berlin.de>
2020-04-11 12:59:07 +0200

    removed merge conflict

--- virusPropagationModel.py
+++ virusPropagationModel.py
@@ -90,7 +90,6 @@ class Simulation(object):
         s_t = self.simulation_timecourse
         s_t.loc[:, 'ones'] = np.ones(s_t.shape[0])
         for status in statuses:
-<<<<<<< HEAD
             df = s_t[s_t['status'] == status]
             gdf = df.groupby('time')
             stat_t = gdf.sum()['ones']
@@ -98,15 +97,6 @@ class Simulation(object):
             df.columns = ['time', status]
             status_trajectories[status] = df
     return status_trajectories
-=======
-            status_trajectories[status] = [(t, len(self.simulation_timecourse[(self.simulation_timecourse['time'] == t)
-                                                                              & (self.simulation_timecourse[
-                                                                                 'status'] == status)]))
-                                           for t in self.simulation_timecourse['time'].unique()]
-            status_trajectories[status] = pd.DataFrame(
-                status_trajectories[status], columns=['time', status])
-        return status_trajectories
->>>>>>> 680170efde56d15f7b805e3cd46de7eb1344cd37
 
     def plot_status_timecourse(self, specific_statuses=None, save_figure=False):
         """
