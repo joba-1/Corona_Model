f1b35276c08371bb97fc4ccb3cba3db9124e4f64
jwodke <judith.wodke@hu-berlin.de>
2020-04-22 15:05:22 +0200

    now also printing the number of unique connections per person at a given time point

--- generate_edge_file_from_output.py
+++ generate_edge_file_from_output.py
@@ -8,7 +8,7 @@
 ## - input file for cytoscape (defining unique connections between people) and their count ##
 ## For teh moment this is limited to 24 hours, since daily schedules for people do not     ##
 ## change.                                                                                 ##
-## (c) Björn Goldenbogen, Judith Wodke                                                     ##
+## (c) Bjoern Goldenbogen, Judith Wodke                                                     ##
 #############################################################################################
 
 ## import libraries
@@ -20,7 +20,8 @@ import sys
 def GetOptions(args=sys.argv[1:]):
     parser = argparse.ArgumentParser(description="Parses command.")
     parser.add_argument("-if", "--inputfile", type=str, help="Input File (default datafiles/sim.csv)")
-    parser.add_argument("-of", "--outputfile", type=str, help="Input File (default edges.csv)")
+    parser.add_argument("-of", "--outputfile", type=str, help="Output File, should contain 'edges' (default edges.csv)")
+    parser.add_argument("-st", "--simtime", type=int, help="Simulation Time (default end time of sim.csv)")
     #parser.add_argument("-n", "--number", type=int, help="A number.")
     #parser.add_argument("-v", "--verbose",dest='verbose',action='store_true', help="Verbose mode.")
     options = parser.parse_args(args)
@@ -33,32 +34,42 @@ options = GetOptions(sys.argv[1:])
 if options.inputfile:
     i_f = options.inputfile
 else:
-    i_f  = 'sim.csv'
+    i_f  = './datafiles/sim.csv'
 
 if options.outputfile:
-	o_f = options.outputfile
+    o_f = options.outputfile
+    tmp = options.outputfile.split('edges')
+    o_f_2 = tmp[0] + 'nodes' + tmp[1]
 else:
-	o_f  = 'edges.csv'
+    o_f  = 'edges.csv'
+    o_f_2 = 'nodes.csv'
 
 ## read in the input file
 df =pd.read_csv(i_f)
 
 ## extract list of timepoints
-times = df['time'].unique()
-times = times[:24] ## limit time points to first 24 (NOTE: think about if we want this per day simulated at a certain point in time)
+times = sorted(df['time'].unique())
+if options.simtime:
+    final_time = options.simtime ## set final time to input argument value if given
+else:
+    final_time = times[-1] ## set final time to last timestep in times
+times = times[:final_time] ## limit time points to final_time
+
 ## extract list of locations
 locations = df['loc'].unique()
 ## initialize dicts and lists
 counted_connections = {}
 status_at_final_time = {}
-edges=[]
+human_unique_connections = {}
+edges = []
+nodes = []
 
 ## iterate over times and locations to identify connections between pairs of people and count them
 for t in times:
     for location in locations:
         loc_at_time=df[(df['time']==t)&(df['loc']==location)] ## extract location at time
         humans_in_loc = list(sorted(loc_at_time['h_ID'].values)) ## extract sorted list of humans in location
-        df[(df['time']==t)&(df['loc']==length(times))] ## what do we do this for???
+        #df[(df['time']==t)&(df['loc']==len(times))] ## what do we do this for???
         h_l = humans_in_loc[:] ## get real copy of humans in loc list
         for human in humans_in_loc:
             h_l.remove(human) ## remove human from humnas in loc list copy to only count pairs once
@@ -71,6 +82,9 @@ for t in times:
                 else:
                     counted_connections[(human, human_out)] = 1 ## otherwise set to 1
 
+for pers in status_at_final_time.keys():
+    human_unique_connections[pers] = 0
+
 ## for each unique pair define the connection (edge in cytoscape)
 for pair in counted_connections.keys():
     persA = pair[0] ## entry node
@@ -78,8 +92,17 @@ for pair in counted_connections.keys():
     statusA = status_at_final_time[persA] ## status at t=final of entry node
     statusB = status_at_final_time[persB] ## status at t=final of exit node
     edge={'persID_A': persA, 'persID_B': persB, 'Edge_type':'pp', 'status_persA': statusA, 'status_persB': statusB,
-          'time': t, 'number_connections': counted_connections[pair]} ## define edge
+          'time': t, 'number_connections': counted_connections[pair]} ## define edge for output
     edges.append(edge) ## append edge to edges list
+    human_unique_connections[persA] += 1
+    human_unique_connections[persB] += 1
+
+print('status keys: ' + str(len(status_at_final_time.keys())))
+print('humans with connctions: ' + str(len(human_unique_connections.keys())))
+
+for pers in status_at_final_time.keys():
+    node = {'pers_ID': pers, 'final_time': final_time, 'status': status_at_final_time[pers], 'number_of_unique_connections': human_unique_connections[pers]}
+    nodes.append(node)
 
 ## print cytoscape input file
 try:
@@ -87,4 +110,11 @@ try:
     df_out.to_csv('datafiles/'+o_f, sep='\t', na_rep='', header=True, index=True, mode='w', quotechar='"', decimal='.')
     print('datafiles/' + o_f + ' saved')
 except:
-    print('could not save')
+    print('could not save' + o_f)
+
+try:
+    df_out2 = pd.DataFrame(nodes)
+    df_out2.to_csv('datafiles/'+o_f_2, sep='\t', na_rep='', header=True, index=True, mode='w', quotechar='"', decimal='.')
+    print('datafiles/' + o_f_2 + ' saved')
+except:
+    print('could not save' + o_f_2)
