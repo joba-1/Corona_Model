3206dac778690fed4f2f9bf6a0444be33cc0fb9b
stephan <adlerste@hu-berlin.de>
2020-04-24 21:19:22 +0200

    pool

--- get_status_data_and_plots_from_parallel.py
+++ get_status_data_and_plots_from_parallel.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
 import sys
-import matplotlib.cm as cm 
-
+import matplotlib.cm as cm
+from multiprocessing import Pool
 
 
 #def getOptions(args=sys.argv[1:]):
@@ -25,30 +25,41 @@ sim_files = [x for x in file_list if x.endswith('pkl')]#and x.startswith('sim')]
 
 status_trajectories_list= []
 
-for filename in sim_files:
+def get_df_list(filename):
     sim = load_simulation_object('scenario_output/'+filename)
-    status_trajectories_list.append(sim.get_status_trajectories())
+    return sim.get_status_trajectories()
 
 
-for i,stat in enumerate(['I','S','R','D']):
+if __name__=='__main__':
 
-    try:
-        df = pd.concat([status_trajectories_list[i][stat].set_index('time') for i in range(len(status_trajectories_list))], axis=1)#, join='outer', join_axes=None, ignore_index=False,
-                  #keys=None, levels=None, names=None, verify_integrity=False,
-                  #copy=True)
-        df.columns = [stat+str(i) for i in range(len(status_trajectories_list))]
-        df.to_csv('outputs/'+filename[:-4]+'_'+stat+'.csv')
-        #df.reset_index().drop('time')
+    start = timeit.default_timer()
 
-        #df_sims.mean(axis=1)
+    with Pool(20) as pool:
+        status_trajectories_list = pool.map(get_df_list, sim_files)
 
-        ax = df.plot(c=cmap(i),alpha=0.2, legend=False)
-        ax.plot()
-        df.mean(axis=1).plot(c='k')
-        ax.set_title(stat); ax.set_ylabel('counts'), ax.set_xlabel('time, h')
+    for i,stat in enumerate(['I','S','R','D']):
 
-        
-        plt.savefig('outputs/plots/'+filename[:-4]+'_'+stat+'.png') 
-        plt.close()
-    except:
-        print(stat+'  is not in list')
+        try:
+            df = pd.concat([status_trajectories_list[i][stat].set_index('time') for i in range(len(status_trajectories_list))], axis=1)#, join='outer', join_axes=None, ignore_index=False,
+                      #keys=None, levels=None, names=None, verify_integrity=False,
+                      #copy=True)
+            df.columns = [stat+str(i) for i in range(len(status_trajectories_list))]
+            df.to_csv('outputs/'+filename[:-4]+'_'+stat+'.csv')
+            #df.reset_index().drop('time')
+
+            #df_sims.mean(axis=1)
+
+            ax = df.plot(c=cmap(i),alpha=0.2, legend=False)
+            ax.plot()
+            df.mean(axis=1).plot(c='k')
+            ax.set_title(stat); ax.set_ylabel('counts'), ax.set_xlabel('time, h')
+
+            
+            plt.savefig('outputs/plots/'+filename[:-4]+'_'+stat+'.png') 
+            plt.close()
+        except:
+            print(stat+'  is not in list')
+
+    stop = timeit.default_timer()
+
+    print('time:  ',stop-start)
