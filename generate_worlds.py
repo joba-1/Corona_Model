97e9642a3868f890b1fcf92862f2e09424e95d6a
bjoern <bjoern.goldenbogen.1@biologie.hu-berlin.de>
2020-04-29 21:46:29 +0200

    **kwargs

--- generate_worlds.py
+++ generate_worlds.py
@@ -1,4 +1,5 @@
 from virusPropagationModel import *
+from VPM_save_and_load import *
 import glob
 import os
 from multiprocessing import Pool
@@ -9,10 +10,10 @@ import sys
 
 def ini_and_save_world(i):
     world = ModeledPopulatedWorld(1000,10, world_from_file=True,
-            geofile_name='datafiles/Buildings_Gangelt_MA_3.csv',
+            geofile_name='datafiles/Buildings_Gangelt_MA_1.csv',
                                      agent_agent_infection=True,
                              automatic_initial_infections=False)#, input_schedules='schedules_v1')
-    world.save('worlds/Gangelt_MA_1_'+str(i), date_suffix=False, folder='/home/basar/corona_simulations/saved_objects/worlds')
+    world.save('Gangelt_MA_1_'+str(i), date_suffix=False, folder=output_folder)#'/home/basar/corona_simulations/saved_objects/worlds/')
     print('worlds/Gangelt_MA_1_'+str(i)+' created')
     #return('worlds/Gangelt_MA_1_'+str(i))
 
@@ -30,7 +31,7 @@ if __name__ == '__main__':
     if options.folder:
         output_folder = options.folder
     else:
-        output_folder = '/home/basar/corona_simulations/saved_objects/worlds'   
+        output_folder = '/home/basar/corona_simulations/saved_objects/worlds/'   
 
     try:
         os.mkdir(output_folder)
@@ -38,5 +39,5 @@ if __name__ == '__main__':
         pass    
 
     
-    with Pool(2) as pool:
-        pool.map(ini_and_save_world,[i for i in range(10)])
+    with Pool(50) as pool:
+        pool.map(ini_and_save_world,[i for i in range(100)])
