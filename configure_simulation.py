a798f3f611c497ab01950671d1030fc0619b96fd
oliver <>
2020-07-28 16:22:31 +0200

    Added definition of probability-rate files to configure_simulation.Simulation_Configuration.__init__()

---
+++ configure_simulation.py
@@ -0,0 +1,14 @@
+import pandas
+
+
+class Simulation_Configuration(object):
+    def __init__():
+        config_file = pandas.read_csv('simulation_configuration.csv', sep=',')
+        self.infectivity_df = config_file.loc['infectivity_df', 'Value']
+        self.recovery_df = config_file.loc['recovery_df', 'Value']
+        self.hospitalisation_df = config_file.loc['hospitalisation_df', 'Value']
+        self.icu_death_risk_df = config_file.loc['icu_death_risk_df', 'Value']
+        self.general_death_risk_df = config_file.loc['general_death_risk_df', 'Value']
+        self.to_icu_df = config_file.loc['to_icu_df', 'Value']
+        self.icu_to_hospital_df = config_file.loc['icu_to_hospital_df', 'Value']
+        self.diagnosis_df = config_file.loc['diagnosis_df', 'Value']
