2644c9309aefaa9dfde8018872a55ade98f589c4
oliver <>
2020-09-11 10:42:22 +0200

    Added loss of immunity option

--- configure_simulation.py
+++ configure_simulation.py
@@ -7,6 +7,7 @@ location_coefficients = dict(zip(list(loc_coeff_df['Location']), list(loc_coeff_
 class Simulation_Configuration(object):
     def __init__(self):
         config_file = pandas.read_csv('simulation_configuration.csv', sep=',', index_col=0)
+        self.immunity_loss_df = config_file.loc['immunity_loss_df', 'File']
         self.infectivity_df = config_file.loc['infectivity_df', 'File']
         self.recovery_from_undiagnosed_df = config_file.loc['recovery_from_undiagnosed_df', 'File']
         self.recovery_from_diagnosed_df = config_file.loc['recovery_from_diagnosed_df', 'File']
@@ -21,6 +22,7 @@ class Simulation_Configuration(object):
         #self.icu_to_hospital_df = config_file.loc['icu_to_hospital_df', 'File']
         self.diagnosis_df = config_file.loc['diagnosis_df', 'File']
 
+        self.immunity_loss_dependency config_file.loc['immunity_loss_df', 'Dependency']
         self.infectivity_dependency = config_file.loc['infectivity_df', 'Dependency']
         self.hospitalisation_dependency = config_file.loc['hospitalisation_df', 'Dependency']
         self.to_icu_dependency = config_file.loc['to_icu_df', 'Dependency']
@@ -35,6 +37,7 @@ class Simulation_Configuration(object):
         self.recovery_from_hospitalized_dependency = config_file.loc['recovery_from_hospitalized_df', 'Dependency']
         self.recovery_from_icu_dependency = config_file.loc['recovery_from_icu_df', 'Dependency']
 
+        self.immunity_loss_age_dependency = config_file.loc['immunity_loss_df', 'Age_dependent']
         self.infectivity_age_dependency = config_file.loc['infectivity_df', 'Age_dependent']
         self.hospitalisation_age_dependency = config_file.loc['hospitalisation_df', 'Age_dependent']
         self.to_icu_age_dependency = config_file.loc['to_icu_df', 'Age_dependent']
