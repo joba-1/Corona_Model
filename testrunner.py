5aef72cf4c203fb745a1d3e5cbc3f4dc4dbd0635
Björn Goldenbogen <bjoern.goldenbogen.1@biologie.hu-berlin.de>
2020-09-25 16:18:06 +0200

    Update testrunner.py

--- testrunner.py
+++ testrunner.py
@@ -75,9 +75,9 @@ class TestVPM(unittest.TestCase):
     def test_r_eff_get_from_obj_and_plot(self):
         self.simulation2 = Simulation(self.modeledWorld1, 500)
         self.simulation2.plot_r_eff(4*24)
-        self.simulation2.export_r_eff_time_course_as_csv(2*24,saved_csv_identifier='testing_stepsize1')
-        self.simulation2.export_r_eff_time_course_as_csv(4*24, saved_csv_identifier='testing_stepsize2')
-        vpm_neta.plot_r_eff_from_csvs_or_sim_object(['testing_stepsize1','testing_stepsize2'])
+        #self.simulation2.export_r_eff_time_course_as_csv(2*24,saved_csv_identifier='testing_stepsize1')
+        #self.simulation2.export_r_eff_time_course_as_csv(4*24, saved_csv_identifier='testing_stepsize2')
+        #vpm_neta.plot_r_eff_from_csvs_or_sim_object(['testing_stepsize1','testing_stepsize2'])
         for file in glob.glob("outputs/testing*"):
             os.remove(file)  # files cleanup
 
