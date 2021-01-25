0e53dd260d8468f20111d3ef4883eea6c1b1612f
jwodke <judith.wodke@hu-berlin.de>
2021-01-25 15:38:14 +0100

    update setup.py according to infos from Ivo

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import os
 import io
 #import sys
@@ -10,11 +8,30 @@ NAME='gerda'
 VERSION='2.0'
 DESCRIPTION='A GEospacially Referenced Demographic Agent-based (GERDA) model for virus (SARS-CoV-2) transmission and disease (COVID-19) progression.'
 LONG_description=''
-AUTHOR=['Björn Goldenbogen', 'Stephan O. Adler', 'Oliver Bodeit', 'Judith A.H. Wodke', 'Aviv Korman', 'Lasse Bonn', 'Johanna E.L. Haffner', 'Maxim Karnetzki', 'Ivo Maintz', 'Martin Seeger']
+AUTHOR=['Björn Goldenbogen', 
+        'Stephan O. Adler', 
+        'Oliver Bodeit', 
+        'Judith A.H. Wodke', 
+        'Aviv Korman', 
+        'Lasse Bonn', 
+        'Johanna E.L. Haffner', 
+        'Maxim Karnetzki', 
+        'Ivo Maintz', 
+        'Martin Seeger']
 EMAIL=['bjoern.goldenbogen@hu-berlin.de','edda.klipp@hu-berlin.de']
 URL='https://tbp-klipp.science/GERDA/code/'
-REQUIRES_PYTHON='>=3.2.0' ## check back with Ivo!
-REQUIRED=['collections', 'copy', 'ctypes', 'glob', 'matplotlib', 'numpy', 'pandas', 'random', 'scipy'] ##external packages as dependencies
+REQUIRES_PYTHON='>=3.6.9' ## with this version everything got tested
+REQUIRED=['gdal==1.9.2', 
+          'geopandas==0.8.1', 
+          'jupyter==1.0.0', 
+          'matplotlib==3.3.2', 
+          'numpy==1.19.2', 
+          'osmnx==0.16.2', 
+          'pandas==1.1.2', 
+          'rtree==0.9.4', 
+          'seaborn==0.11.0', 
+          'scipy==1.5.2',
+          'shapely==1.7.1'] ## external packages as dependencies (versions have been tested, might also work with more recent versions)
 LICENSE='GNU GPL v3.0'
 
 ## check for readme file, otherwise use description as long_description
@@ -25,6 +42,8 @@ try:
 except FileNotFoundError:
     LONG_description = DESCRIPTION
 LONG_description_content_type='text/markdown'
+
+## if setup.py should be used for direct uploading, removal of previous builds should be added
 
 ## create setup using predefined variables
 setup(
@@ -43,7 +62,7 @@ setup(
    )
 
 """
-## what about classifiers, e.g.
+## in case classifiers shall be used, include code similar to the below example:
    classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
