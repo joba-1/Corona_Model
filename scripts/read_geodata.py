98a998f7a7483b219593aca977f9c2ada33c93ed
bjoern <bjoern.goldenbogen.1@biologie.hu-berlin.de>
2021-02-02 13:20:08 +0100

    change save path

--- scripts/read_geodata.py
+++ scripts/read_geodata.py
@@ -142,7 +142,7 @@ try:
     area = ox.gdf_from_place(places[loc][0])
 
     # save gdf as geojason objects
-    area.to_file('Area_'+places[loc][0].split(',')[0].replace(' ', '_') +
+    area.to_file('generate: input_data/geo/Area_'+places[loc][0].split(',')[0].replace(' ', '_') +
              '_MA_'+str(min_area).replace('.', '_')+'.geojson', driver='GeoJSON')
     print('generate: input_data/geo/Area_' +
           places[loc][0].split(',')[0].replace(' ', '_')+'_MA_'+str(min_area).replace('.', '_')+'.geojson')
@@ -153,7 +153,7 @@ try:
     # traffic network
     edges = ox.graph_to_gdfs(graph, nodes=False)
     streets = edges[['access', 'geometry']].copy()  # saving without this caused problems
-    streets.to_file('Streets_'+places[loc][0].split(',')[0].replace(' ', '_') +
+    streets.to_file('generate: input_data/geo/Streets_'+places[loc][0].split(',')[0].replace(' ', '_') +
                     '_MA_'+str(min_area).replace('.', '_')+'.geojson', driver='GeoJSON')
     print('generate: input_data/geo/Streets_' +
           places[loc][0].split(',')[0].replace(' ', '_')+'_MA_'+str(min_area).replace('.', '_')+'.geojson')
@@ -192,10 +192,10 @@ red_buildings['neighbourhood'] = neighbourhoods
 
 locations = exclude_small_buildings(red_buildings, min_area)
 
-locations.to_file('Buildings_'+places[loc][0].split(',')[0].replace(' ', '_') +
+locations.to_file('input_data/geo/Buildings_'+places[loc][0].split(',')[0].replace(' ', '_') +
                   '_MA_'+str(min_area).replace('.', '_')+'.geojson', driver='GeoJSON')
 df = pd.DataFrame(locations)
-df.to_csv('Buildings_'+places[loc][0].split(',')[0].replace(' ',
+df.to_csv('input_data/geo/Buildings_'+places[loc][0].split(',')[0].replace(' ',
                                                             '_')+'_MA_'+str(min_area).replace('.', '_')+'.csv')
 
 print('generate: input_data/geo/Buildings_' +
